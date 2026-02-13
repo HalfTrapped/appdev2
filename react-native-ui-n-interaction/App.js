feaacb9090c9965172f14137be4ca07bb6325853
Aaron Matthew Tulalian <asianwater1@gmail.com>
2026-02-13 14:28:42 +0800

    Done with Style sheet

---
+++ react-native-ui-n-interaction/App.js
@@ -0,0 +1,13 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+import LotsOfStyles from './components/StyleSheet';
+
+export default function App() {
+  return (
+    <View>
+      <LotsOfStyles />
+    </View>
+  );
+}
+
+
