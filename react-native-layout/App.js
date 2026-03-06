af79bea0555e117e0a206e56aa397c5f2b990df8
Aaron Matthew Tulalian <asianwater1@gmail.com>
2026-03-06 19:28:15 +0800

    Added Styles and borders for app areas

--- react-native-layout/App.js
+++ react-native-layout/App.js
@@ -1,12 +1,11 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
-import LoginPage from './src/screen/LoginPage';
+import React from 'react';
+import LoginPage  from './src/screen/LoginPage';
 
 export default function App() {
   return (
-    <View >
       <LoginPage />
-    </View>
   );
 }
 
--- react-native-layout/src/screen/LoginPage.js
+++ react-native-layout/src/screen/LoginPage.js
@@ -3,15 +3,15 @@ import { StyleSheet, Text, View } from 'react-native';
 
 export default function LoginPage() {
   return (
-    <View style={styles.container}>
+   <View style={styles.container}>
       <View style={styles.header}>
-        <Text>header</Text>
+        <Text style={styles.text}>Login Page</Text>
       </View>
       <View style={styles.body}>
-        <Text>Body</Text>
+        <Text style={styles.text}>Body</Text>
       </View>
       <View style={styles.footer}>
-        <Text>Footer</Text>
+        <Text style={styles.text}>Footer</Text>
       </View>
     </View>
   );
@@ -26,18 +26,22 @@ const styles = StyleSheet.create({
     flex: 1,
     alignItems: 'center',
     justifyContent: 'flex-end',
-    marginBottom: 16,
+    marginBottom: 16, 
   },
   body: {
     flex: 1.75,
     alignItems: 'center',
     justifyContent: 'center',
-    fontSize: 16,
     marginBottom: 16,
   },
   footer: {
     flex: 1,
     alignItems: 'center',
     justifyContent: 'flex-start',
+  },
+  text: {
+    color: '#dfecec',
+    fontSize: 15,
+    fontFamily: 'Roboto Bold',
   },
 }); 
