cdaff8f6dbeee3a2cf24feffa25a8a6e817ff43f
Aaron Matthew Tulalian <asianwater1@gmail.com>
2026-03-06 21:03:34 +0800

    added text inputs and login button

--- react-native-layout/src/screen/LoginPage.js
+++ react-native-layout/src/screen/LoginPage.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import SignUp from './SignUpPage';
-import { StyleSheet, Text, View, TouchableOpacity, Button} from 'react-native';
+import { StyleSheet, Text, View, TouchableOpacity, TextInput} from 'react-native';
 import { useNavigation } from '@react-navigation/native';
 
 export default function LoginPage({navigation}) {
@@ -11,17 +11,26 @@ export default function LoginPage({navigation}) {
       </View>
       <View style={styles.body}>
         <Text style={styles.text}>Body</Text>
+
+        <View style={{alignSelf: 'flex-start', marginLeft: 40}}>
+          <Text style={{color: 'black', marginTop: 5, fontSize: 15}}>E-mail</Text>
+        </View>
+        <TextInput style={[styles.textInput, {color: 'black'}]} placeholder="Enter your Email"/>
+
+        <View style={{alignSelf: 'flex-start', marginLeft: 40}}>
+          <Text style={{color: 'black', marginTop: 5, fontSize: 15}}>Password</Text>
+        </View>
+        <TextInput style={[styles.textInput, {color: 'black'}]} placeholder="Enter your Password" secureTextEntry />
+
         <TouchableOpacity>
-          <Text style={styles.loginButton}>Email</Text>
-        </TouchableOpacity>
-        <TouchableOpacity>
-          <Text style={styles.loginButton}>Password</Text>
-        </TouchableOpacity>
+          <Text style={[styles.loginButton, {color: 'white'}]}>Log In Button</Text>
+        </TouchableOpacity> 
         <TouchableOpacity>
           <Text style={styles.text}>Already have an account?
             <Text style={styles.signUpText} onPress={() => navigation.navigate('SignUp')}> Sign Up</Text>
           </Text>
         </TouchableOpacity>
+        
       </View>
       <View style={styles.footer}>
         <Text style={styles.text}>Footer</Text>
@@ -35,6 +44,7 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: '#2f4f4f',
   },
+ 
   header: {
     flex: 1,
     alignItems: 'center',
@@ -47,7 +57,7 @@ const styles = StyleSheet.create({
     flex: 1.75,
     alignItems: 'center',
     justifyContent: 'center',
-    marginBottom: 16,
+    marginBottom: 15,
     borderColor: '#dfecec',
     borderWidth: 1,
   },
@@ -67,16 +77,27 @@ const styles = StyleSheet.create({
     marginTop: 20,
     paddingVertical: 10,
     paddingHorizontal: 100,
-    borderRadius: 5,
     marginHorizontal: 20,
     marginBottom: 16,
-    borderColor: '#dfecec',
-    borderWidth: 1,
+    borderColor: 'rgb(197, 199, 209)',
+    borderWidth: 1.5,
+    borderRadius: 15,
+    width: '80%',
+    backgroundColor: 'rgb(241, 186, 33)',
   },
   signUpText: {
     backgroundColor: 'rgba(255, 255, 255, 0.2)',
     color: '#dfecec',
     fontSize: 15,
     fontFamily: 'Roboto Bold',
+  },
+   textInput: {
+    backgroundColor: 'rgb(234, 245, 243)',
+    marginTop: 10,
+    paddingVertical: 10,
+    borderColor: 'rgb(110, 113, 124)',
+    borderWidth: 1.5,
+    borderRadius: 10,
+    width: '80%',
   },
 }); 
