e94b499624742ebeba9e85ae353ac35f119551ed
HalfTrapped <asianwater1@gmail.com>
2026-03-07 10:55:46 +0800

    step 1: copy loginscreen and rename to signupscreen

---
+++ appdev2-midterm-exam/src/screen/SignupScreen.js
@@ -0,0 +1,143 @@
+import {
+  StyleSheet,
+  Text,
+  View,
+  Image,
+  TouchableOpacity,
+  TextInput,
+} from "react-native";
+import Ionicons from "@react-native-vector-icons/ionicons";
+
+export default function SignupScreen() {
+  return (
+    <View style={styles.container}>
+      {/* 1. Header Section */}
+      <View style={styles.header}>
+        <Image
+          source={require("../../assets/Signup.webp")}
+          style={styles.image}
+        />
+      </View>
+
+      {/* 2. Form Section */}
+      <View style={styles.formContainer}>
+        <Text style={styles.label}>Full Name</Text>
+        <TextInput style={styles.input} placeholder="John Doe" />
+
+        <Text style={styles.label}>Email Address</Text>
+        <TextInput style={styles.input} placeholder="john@gmail.com" />
+
+        <Text style={styles.label}>Password</Text>
+        <TextInput style={styles.input} placeholder="********"  secureTextEntry  />
+
+        <TouchableOpacity style={styles.loginButton}>
+            <Text style={styles.loginButtonText}>Sign Up</Text>
+        </TouchableOpacity>
+
+        <Text style={styles.orText}>Or</Text>
+
+        <View style={styles.socialRow}>
+          <TouchableOpacity style={styles.socialIcon}>
+            <Ionicons name="logo-google" size={30} color="#DB4437" />
+          </TouchableOpacity>
+
+          <TouchableOpacity style={styles.socialIcon}>
+            <Ionicons name="logo-facebook" size={30} color="black" />
+          </TouchableOpacity>
+
+          <TouchableOpacity style={styles.socialIcon}>
+            <Ionicons name="logo-apple" size={30} color="#4267B2" />
+          </TouchableOpacity>
+        </View>
+
+        <View style={styles.footer}>
+          <Text>Already have an account? </Text>
+          <TouchableOpacity>
+            <Text style={styles.linkText}>Log In</Text>
+          </TouchableOpacity>
+        </View>
+      </View>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+    container:{
+        flex: 1,
+        backgroundColor: '#7D7AFF',
+        paddingTop: 40,
+    },
+    header:{
+        flex: 1,
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+    image: {
+      width: '80%',
+      height: '70%',
+    },
+    formContainer: {
+      flex: 3,
+      backgroundColor: '#FFF',
+      borderTopLeftRadius: 60,
+      borderTopRightRadius: 60,
+      padding:30,
+    },
+    label:{
+      fontSize: 14,
+      marginBottom: 5,
+      marginTop: 15,
+      color: '#666',
+
+    },
+    input: {
+      backgroundColor: '#F0F0F0',
+      borderRadius: 15,
+      padding: 15,
+      fontSize: 16,
+    },
+    forgotText: {
+      alignSelf: 'flex-end',
+      color: '#666',
+      marginTop: 10,
+    },
+    loginButton: {
+      backgroundColor: '#FFCC00',
+      borderRadius: 15,
+      padding: 18,
+      alignItems: 'center',
+      marginTop: 30,
+    },
+    loginButtonText: {
+      fontWeight: 'bold',
+      fontSize: 18,
+    },
+    orText: {
+      textAlign: 'center',
+      marginVertical: 20,
+      fontSize: 18,
+      fontWeight: 'bold',
+    },
+    socialRow: {
+      flexDirection: 'row',
+      justifyContent: 'center',
+      gap: 20,
+    },
+    socialIcon: {
+      backgroundColor: '#F0F0F0',
+      borderRadius: 15,
+      padding: 15,
+    },
+    footer: {
+        flex:1,
+        flexDirection: 'row',
+        justifyContent: 'center',
+        marginTop: 30,
+    },
+    linkText: {
+      color: '#FFCC00',
+      fontWeight: 'bold',
+    },
+
+});
+
