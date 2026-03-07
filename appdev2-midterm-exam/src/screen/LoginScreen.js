bea1896e25b989d389bcab1178483ed2a12a23f1
HalfTrapped <asianwater1@gmail.com>
2026-03-07 10:53:18 +0800

    step 8 create the or separator

--- appdev2-midterm-exam/src/screen/LoginScreen.js
+++ appdev2-midterm-exam/src/screen/LoginScreen.js
@@ -113,4 +113,10 @@ const styles = StyleSheet.create({
       fontWeight: 'bold',
       fontSize: 18,
     },
+    orText: {
+      textAlign: 'center',
+      marginVertical: 20,
+      fontSize: 18,
+      fontWeight: 'bold',
+    },
 });
